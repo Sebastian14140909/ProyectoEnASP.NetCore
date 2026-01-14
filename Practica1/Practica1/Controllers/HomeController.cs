b3478b01b1fe5e31db9f362848bb648de7c5bc69
Sebas090909 <133933733+Sebastian14140909@users.noreply.github.com>
2026-01-13 20:22:53 -0600

    Fecha del sistema y nombre del curso
    Se agrega la fecha del sistema y nombre del curso

--- Practica1/Practica1/Controllers/HomeController.cs
+++ Practica1/Practica1/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.FechaActual = DateTime.Now;
         return View();
     }
 
--- Practica1/Practica1/obj/Debug/net8.0/Practica1..AssemblyInfo.cs
+++ Practica1/Practica1/obj/Debug/net8.0/Practica1..AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica1.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83316a451907a76d6ffa9f9b1a05fed0410a232e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702bc1aad4516a726c2f371c9599dd6a71e83b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica1.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica1.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
